0ead6620ceeb3c4af26704c4aeb15d2ffca0cb4b
MBaKu62 <l.c.chavatte@gmail.com>
2026-03-05 15:42:34 +0100

    Add files via upload

--- APP/sw.js
+++ APP/sw.js
@@ -2,8 +2,29 @@ const CACHE_NAME = "permis-ce-cache-v1";
 const urlsToCache = [
   "/index.html",
   "/css/style.css",
-  "/js/script.js"
-  // ne rien mettre de externe ici
+  "/js/script.js",
+  "/acceuil/feuille-notation",
+  "/fiche-ecrite/5.5",
+  "/fiche-ecrite/50-km-h-remorque", 
+  "/fiche-ecrite/70-5,5t", 
+  "/fiche-ecrite/acces-interdit-explosif",  
+  "/fiche-ecrite/accotement-meuble", 
+  "/fiche-ecrite/arbres-inclines", 
+  "/fiche-ecrite/carrefour-giratoire", 
+  "/fiche-ecrite/descente-dangereuse", 
+  "/fiche-ecrite/deviation", 
+  "/fiche-ecrite/interdiction-de-tourner-a-gauche-vehicule-marchandise", 
+  "/fiche-ecrite/interdiction-vehicule-plus-de-230-m", 
+  "/fiche-ecrite/panneau_obligation_voie_bus", 
+  "/fiche-ecrite/panneau-camion-interdiction-de-depasser", 
+  "/fiche-ecrite/panneau-essieu", 
+  "/fiche-ecrite/panneau-hauteur", 
+  "/fiche-ecrite/polluant", 
+  "/fiche-ecrite/transport-produit-dangereux", 
+  "/fiche-ecrite/vehicule-10m", 
+  "/fiche-ecrite/voie-de-detresse-a-droite", 
+  "/icons/icon-192",
+  "/icons/icon-512",
 ];
 
 self.addEventListener("install", event => {
